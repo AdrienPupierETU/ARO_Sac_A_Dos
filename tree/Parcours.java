2e8a856125ced66fa0c690614338435503aa21e7
AdrienPupierETU <43185374+AdrienPupierETU@users.noreply.github.com>
2019-09-24 14:26:56 +0200

    Update Parcours.java

--- tree/Parcours.java
+++ tree/Parcours.java
@@ -1,21 +1,29 @@
 package tree;
 
+import algorithm.Algorithm;
 import algorithm.Item;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Parcours {
-    public List<Item> MaxState;
+    public List<Boolean> MaxState= new ArrayList<>();
     public int MaxValue=0;
 
     public void parcoursWithBound(List<Item> items,double weightRemaining,int height){
         if(items.size()-1>=height){ //node
             if(weightRemaining-items.get(height).weight>0){
                 items.get(height).setTaken(1);
-                parcoursWithBound(items,(weightRemaining-items.get(height).weight),height+1); //left
+                if(Algorithm.OptimumWithHeight(items,weightRemaining,height)>MaxValue){
+                    parcoursWithBound(items,(weightRemaining-items.get(height).weight),height+1); //left
+                }
+
             }
             items.get(height).setTaken(2);
-            parcoursWithBound(items,weightRemaining,height+1); //right
+            if(Algorithm.OptimumWithHeight(items,weightRemaining,height)>MaxValue){
+                parcoursWithBound(items,weightRemaining,height+1); //right
+            }
+
         }else{ //feuille
             int value=0;
             for(Item i: items){
@@ -24,7 +32,18 @@ public class Parcours {
                 }
             }
             if(MaxValue<value){
-                MaxState=items;
+                if(MaxState.size()!=items.size()){
+                    for(int i =0;i<items.size();i++){
+                        MaxState.add(false);
+                    }
+                }
+                for(int i =0;i<items.size();i++){
+                    if(items.get(i).getTaken()==1){
+                        MaxState.set(i,true);
+                    }else {
+                        MaxState.set(i, false);
+                    }
+                }
                 MaxValue=value;
             }
         }
